4e23abedf08d4eb5a206fae3553953cc56817817
Seung Lee <shl7@uw.edu>
2022-07-22 08:56:14 -0700

    Add initialization tests for Order

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -16,8 +16,36 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order order = new Order();
+      Order order = new Order("", "", 0, new int[1] { 0 });
       Assert.AreEqual(typeof(Order), order.GetType());
+    }
+
+    [TestMethod]
+    public void OrderConstructor_InitializeOrderTitle_Order()
+    {
+      Order order = new Order("Croissants", "", 0, new int[1] { 0 });
+      Assert.AreEqual("Croissants", order.Title);
+    }
+
+    [TestMethod]
+    public void OrderConstructor_InitializeOrderDesc_Order()
+    {
+      Order order = new Order("Croissants", "12 Nutella filled, 12 strawberry jam filled", 0, new int[1] { 0 });
+      Assert.AreEqual("12 Nutella filled, 12 strawberry jam filled", order.Desc);
+    }
+
+    [TestMethod]
+    public void OrderConstructor_InitializeOrderPrice_Order()
+    {
+      Order order = new Order("Croissants", "12 Nutella filled, 12 strawberry jam filled", 72, new int[1] { 0 });
+      Assert.AreEqual(72, order.Price);
+    }
+
+    [TestMethod]
+    public void OrderConstructor_InitializeOrderDate_Order()
+    {
+      Order order = new Order("Croissants", "12 Nutella filled, 12 strawberry jam filled", 72, new int[3] { 2022, 7, 22 });
+      Assert.AreEqual(new DateTime(2022, 7, 22), order.Date);
     }
   }
 }
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,9 +5,19 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
-    public Order()
-    {
+    public string Title { get; }
+    public string Desc { get; }
+    public int Price { get; }
+    public DateTime Date { get; }
 
+    // date consists of length 3 array that holds
+    // { year, month, day }
+    public Order(string title, string desc, int price, int[] date)
+    {
+      Title = title;
+      Desc = desc;
+      Price = price;
+      Date = new DateTime(date[0], date[1], date[2]);
     }
   }
 }
